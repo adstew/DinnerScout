88d9ce9202fe15a9a5f60905392cdca44f2198d2
Aaron <aaronx07@yahoo.com>
2019-10-19 23:37:34 -0400

    Yelp api (needs work)

--- app/src/main/java/test/company/dev/DinnerScout/MainMenu.kt
+++ app/src/main/java/test/company/dev/DinnerScout/MainMenu.kt
@@ -12,9 +12,28 @@ import android.support.v4.app.NotificationCompat
 import android.support.v4.app.NotificationManagerCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.yelp.fusion.client.connection.interceptors.ApiKeyInterceptor;
+import com.yelp.fusion.client.exception.ErrorHandlingInterceptor;
+import com.yelp.fusion.client.models.ApiKey;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
 
 
 import test.company.dev.DinnerScout.fragments.SignupFragment
+import com.yelp.fusion.client.connection.YelpFusionApiFactory
+import com.yelp.fusion.client.models.SearchResponse
+import android.support.v4.app.SupportActivity
+import android.support.v4.app.SupportActivity.ExtraData
+import android.support.v4.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import com.yelp.fusion.client.models.Business
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 class MainMenu : AppCompatActivity(), View.OnClickListener,
     SignupFragment.OnFragmentInteractionListener, LoginFragment.OnFragmentInteractionListener{
@@ -22,14 +41,46 @@ class MainMenu : AppCompatActivity(), View.OnClickListener,
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-
+    companion object {
+        val API_KEY : String =  "M0CMWi9fgorumQJ8FK5oEL7k8npWFFovJd7LP0WQ55zjcVO1Nkkiai_iaWWAuTKv5Qu-ZD3oUG4gprEAnrNo7GJxNUGK47vcWdbp1WDYPDjvRj8YA-bGcepEQ62rXXYx"
+//        val NOTIFICATION_ID = 1
+//        var uid: String? = null
+//        var appsMonitoredKey: String? =
+//            null    // allows creation of user unique sharedpref for checkboxes
+    }
     private var manager: FragmentManager? = null
     private val trans: FragmentTransaction? = null
-
+     var business : Business? = null
+    var params:HashMap<String, String> = HashMap()
+    var apiFactory = YelpFusionApiFactory()
+    var yelpFusionApi = apiFactory.createAPI(API_KEY)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        inflateFirebaseLogin()
 
+
+        params.put("latitude",  "40.581140")
+        params.put("longitude","-111.914184")
+//        val call = yelpFusionApi.getBusinessSearch(params)
+//        val response = call.execute()
+        var a = 1 + 1
+
+        val call = yelpFusionApi.getBusiness("gR9DTbKCvezQlqvD7_FzPw");
+        call.enqueue(object : Callback<Business> {
+
+            override fun onResponse(call : Call<Business>?, response : Response<Business> ) {
+                if(response != null && response.isSuccessful) {
+                     business = response.body()
+                     var a  = 1 + 1
+                }
+                // Update UI text with the Business object.
+
+            }
+            override fun onFailure(call: Call<Business>?, t: Throwable?) {
+                // HTTP error happened, do something to handle it.
+            }
+        })
         // Initialize view variables with respective IDs.
 //        settingsButton = findViewById(R.id.settingsButton)
 //        aboutButton = findViewById(R.id.aboutButton)
@@ -38,7 +89,6 @@ class MainMenu : AppCompatActivity(), View.OnClickListener,
 
 
         // Inflates Firebase login
-        inflateFirebaseLogin()
 
         // creates notification bar
 //        createNotificationChannel()
@@ -86,7 +136,15 @@ class MainMenu : AppCompatActivity(), View.OnClickListener,
         //editor3.putString("userID", uid);
         //editor3.commit();
 
-    }
+
+
+
+}
+
+
+
+
+
 
 
 
@@ -172,11 +230,5 @@ class MainMenu : AppCompatActivity(), View.OnClickListener,
         return mode == AppOpsManager.MODE_ALLOWED
     }
 
-    companion object {
 
-        val NOTIFICATION_ID = 1
-        var uid: String? = null
-        var appsMonitoredKey: String? =
-            null    // allows creation of user unique sharedpref for checkboxes
-    }
 }
