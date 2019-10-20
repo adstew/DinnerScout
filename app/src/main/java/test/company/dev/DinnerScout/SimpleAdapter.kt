5872b5094342742591be4a7e6018e00392efbb9c
Javier Salas <JavierTSalas@users.noreply.github.com>
2019-10-20 07:39:18 -0400

    Added PreferencesFragment for the user to set their search parameters. Data is written to SharedPreferences

---
+++ app/src/main/java/test/company/dev/DinnerScout/SimpleAdapter.kt
@@ -0,0 +1,44 @@
+package  test.company.dev.DinnerScout
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.row_item.view.*
+
+class SimpleAdapter(private val items: MutableList<String>) :
+    RecyclerView.Adapter<SimpleAdapter.VH>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        return VH(parent)
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    fun addItem(name: String) {
+        items.add(name)
+        notifyItemInserted(items.size)
+    }
+
+    fun removeAt(position: Int) {
+        items.removeAt(position)
+        notifyItemRemoved(position)
+    }
+
+    fun getItems(): MutableList<String> {
+        return items
+    }
+
+    class VH(parent: ViewGroup) : RecyclerView.ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.row_item, parent, false)
+    ) {
+
+        fun bind(name: String) = with(itemView) {
+            rowName.text = name
+        }
+    }
+}
